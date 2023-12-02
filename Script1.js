eea43b50b89a6cbaf514bae28a978b5d4f962602
Polocrafting367 <152706679+Polocrafting367@users.noreply.github.com>
2023-12-02 17:41:18 +0100

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,32 @@
+    function changerFichierCSS367() {
+        var styleSheet = document.getElementById("styleSheet");
+        styleSheet.href = "https://cdn.jsdelivr.net/gh/Polocrafting367/WindowsBlurry-1/WB367.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
+                var bouton = document.getElementById("download");
+        bios.style.display = "none";
+        bts.style.display = "none";
+    }
+        function changerFichierCSSXP() {
+        var styleSheet = document.getElementById("styleSheet");
+        styleSheet.href = "https://cdn.jsdelivr.net/gh/Polocrafting367/WindowsBlurry-1/WXP.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
+                var bouton = document.getElementById("download");
+        bios.style.display = "none";
+        bts.style.display = "none";
+    }
+
+    function phone(){
+        off.style.display = "none";
+        offIsVisible = false;
+        slidingDiv.style.display = "none";
+        rectangle.style.display = "block";
+        rectangleIsVisible = false;
+                ba.style.display = "none";
+        B1IsVisible = false;
+                        bb.style.display = "none";
+        B2IsVisible = false;
+        var styleSheet = document.getElementById("styleSheet");
+
+        styleSheet.href = "https://cdn.jsdelivr.net/gh/Polocrafting367/WindowsBlurry-1/WP.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
+        bts.style.display = "none";
+        bios.style.display = "none";
+        var bouton = document.getElementById("download");
+}
