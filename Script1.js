5971198413bf241d4d1915c4ad15203a7a197ffb
Polocrafting367 <152706679+Polocrafting367@users.noreply.github.com>
2023-12-02 18:43:32 +0100

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -1,18 +1,20 @@
 
     function changerFichierCSS367() {
         var styleSheet = document.getElementById("styleSheet");
-        styleSheet.href = "https://unpkg.com/browse/Polocrafting367/WindowsBlurry-1@main/WB367.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
+        styleSheet.href = "https://raw.githack.com/Polocrafting367/WindowsBlurry-1/main/WB367.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
                 var bouton = document.getElementById("download");
         bios.style.display = "none";
         bts.style.display = "none";
     }
         function changerFichierCSSXP() {
         var styleSheet = document.getElementById("styleSheet");
-        styleSheet.href = "https://unpkg.com/browse/Polocrafting367/WindowsBlurry-1@main/WXP.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
+        styleSheet.href = "https://raw.githack.com/Polocrafting367/WindowsBlurry-1/main/WXP.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
                 var bouton = document.getElementById("download");
         bios.style.display = "none";
         bts.style.display = "none";
     }
+
+
 
     function phone(){
         off.style.display = "none";
@@ -26,7 +28,7 @@
         B2IsVisible = false;
         var styleSheet = document.getElementById("styleSheet");
 
-        styleSheet.href = "https://unpkg.com/browse/Polocrafting367/WindowsBlurry-1@main/WP.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
+        styleSheet.href = "https://raw.githack.com/Polocrafting367/WindowsBlurry-1/main/WP.css"; // Remplacez "NouveauFichier.css" par le nom de votre nouveau fichier CSS
         bts.style.display = "none";
         bios.style.display = "none";
         var bouton = document.getElementById("download");
