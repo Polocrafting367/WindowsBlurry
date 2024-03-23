ff88a49702f7da49b64c3431b2996338080d03f8
Polocrafting <jeuxtutovideo12@gmail.com>
2024-03-23 14:45:03 +0100

    f
    Co-Authored-By: Polocrafting 367 <Polocrafting.pro@gmail.com>
    Co-Authored-By: Polocrafting <152706679+polocrafting367@users.noreply.github.com>

---
+++ Work/Rais.js
@@ -0,0 +1,4 @@
+const lieuData = {
+    types: ["Type 1", "Type 2", "Type 3"], // Remplacez les éléments par les types réels que vous souhaitez afficher
+    causes: ["Cause 1", "Cause 2", "Cause 3"] // Remplacez les éléments par les causes réelles que vous souhaitez afficher
+};
