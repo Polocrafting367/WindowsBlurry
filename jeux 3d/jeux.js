9962418cc703f7640e4efb4fd427f63808fd7dc7
Polocrafting <jeuxtutovideo12@gmail.com>
2024-05-01 22:16:15 +0200

    4

--- jeux 3d/jeux.js
+++ jeux 3d/jeux.js
@@ -15,8 +15,10 @@ let frameCount = 0;
 let fpsElement = document.getElementById('fps');
 let coordinatesElement = document.getElementById('coordinates');
 let accelerationElement = document.getElementById('acceleration');
-let probaennemy = 0.15; // Vitesse initiale
-
+let probaennemy = 0.02; // Vitesse initiale
+let chronoRunning = false;
+let startTime;
+let endTime;
 
 const TILE_SIZE = 5;
 function init() {
@@ -35,7 +37,7 @@ function init() {
     const characterMaterial = new THREE.MeshBasicMaterial({ map: characterTexture });
     const characterGeometry = new THREE.BoxGeometry(1, 2, 1); // Dimensions du personnage
     character = new THREE.Mesh(characterGeometry, characterMaterial);
-    character.position.set(25, 0, 0);
+    character.position.set(25, 0, -20);
     scene.add(character);
 
     // Gestion des événements clavier
@@ -85,21 +87,23 @@ function loadSceneFromFile(filename, onLoadCallback) {
                                 enemies.push(enemy);
 
                             }
-                            break;
-                        case 'B': // Immeuble grand
-                            const buildingTextureBig = new THREE.TextureLoader().load('building_big.png');
-                            const buildingMaterialBig = new THREE.MeshBasicMaterial({ map: buildingTextureBig });
-                            const buildingGeometryBig = new THREE.BoxGeometry(TILE_SIZE, TILE_SIZE * 2, TILE_SIZE);
-                            object = new THREE.Mesh(buildingGeometryBig, buildingMaterialBig);
-                            object.userData.type = 'building'; // Définir le type de l'objet comme "building"
-                            break;
-                        case 'A': // Immeuble grand
-                            const buildingTexturefinish = new THREE.TextureLoader().load('building_finish.png');
-                            const buildingMaterialfinish = new THREE.MeshBasicMaterial({ map: buildingTexturefinish });
-                            const buildingGeometryfinish = new THREE.BoxGeometry(TILE_SIZE, TILE_SIZE * 2, TILE_SIZE);
-                            object = new THREE.Mesh(buildingGeometryfinish, buildingMaterialfinish);
-                            object.userData.type = 'building'; // Définir le type de l'objet comme "building"
-                            break;
+                          break;
+                            case 'B': // Immeuble grand (type 'building')
+                                const buildingTextureBig = new THREE.TextureLoader().load('building_big.png');
+                                const buildingMaterialBig = new THREE.MeshBasicMaterial({ map: buildingTextureBig });
+                                const buildingGeometryBig = new THREE.BoxGeometry(TILE_SIZE, TILE_SIZE * 2, TILE_SIZE);
+                                object = new THREE.Mesh(buildingGeometryBig, buildingMaterialBig);
+                                object.userData.type = 'building'; // Définir le type de l'objet comme "building"
+                                break;
+
+                            case 'A': // Immeuble de fin (type 'END')
+                                const buildingTexturefinish = new THREE.TextureLoader().load('building_finish.png');
+                                const buildingMaterialfinish = new THREE.MeshBasicMaterial({ map: buildingTexturefinish });
+                                const buildingGeometryfinish = new THREE.BoxGeometry(TILE_SIZE, TILE_SIZE * 2, TILE_SIZE);
+                                object = new THREE.Mesh(buildingGeometryfinish, buildingMaterialfinish);
+                                object.userData.type = 'END'; // Définir le type de l'objet comme "END"
+                                break;
+
                         case 'b': // Immeuble petit
                             const buildingTextureSmall = new THREE.TextureLoader().load('building_small.png');
                             const buildingMaterialSmall = new THREE.MeshBasicMaterial({ map: buildingTextureSmall });
@@ -108,7 +112,7 @@ function loadSceneFromFile(filename, onLoadCallback) {
                             object.userData.type = 'building'; // Définir le type de l'objet comme "building"
                             break;
                         case '-': // Route horizontale
-                            const roadTextureHorizontal = new THREE.TextureLoader().load('road_horizontal.png');
+                            const roadTextureHorizontal = new THREE.TextureLoader().load('0.png');
                             const roadMaterialHorizontal = new THREE.MeshBasicMaterial({ map: roadTextureHorizontal, transparent: true });
                             const roadGeometryHorizontal = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
                             object = new THREE.Mesh(roadGeometryHorizontal, roadMaterialHorizontal);
@@ -119,8 +123,8 @@ function loadSceneFromFile(filename, onLoadCallback) {
 
                             }
                             break;
-                        case '|': // Route verticale
-                            const roadTextureVertical = new THREE.TextureLoader().load('road_vertical.png');
+                        case '1': // Route verticale
+                            const roadTextureVertical = new THREE.TextureLoader().load('1.png');
                             const roadMaterialVertical = new THREE.MeshBasicMaterial({ map: roadTextureVertical, transparent: true });
                             const roadGeometryVertical = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
                             object = new THREE.Mesh(roadGeometryVertical, roadMaterialVertical);
@@ -131,11 +135,72 @@ function loadSceneFromFile(filename, onLoadCallback) {
 
                             }
                             break;
-                        case '+': // Croisement de routes
-                            const roadTextureCross = new THREE.TextureLoader().load('road_cross.png');
+                        case '2': // Croisement de routes
+                            const roadTextureCross = new THREE.TextureLoader().load('2.png');
                             const roadMaterialCross = new THREE.MeshBasicMaterial({ map: roadTextureCross, transparent: true });
                             const roadGeometryCross = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
                             object = new THREE.Mesh(roadGeometryCross, roadMaterialCross);
+                            object.rotation.x = -Math.PI / 2; // Aligner le croisement avec le sol
+                            if (Math.random() <= probaennemy) { // 20% de probabilité
+                                const enemy = createEnemy(j * TILE_SIZE + TILE_SIZE / 2, -i * TILE_SIZE - TILE_SIZE / 2);
+                                enemies.push(enemy);
+
+                            }
+                            break;
+                        case '3': // Croisement de routes
+                            const roadTextureCrossspire = new THREE.TextureLoader().load('3.png');
+                            const roadMaterialCrossspire = new THREE.MeshBasicMaterial({ map: roadTextureCrossspire, transparent: true });
+                            const roadGeometryCrossspire = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
+                            object = new THREE.Mesh(roadGeometryCrossspire, roadMaterialCrossspire);
+                            object.rotation.x = -Math.PI / 2; // Aligner le croisement avec le sol
+                            if (Math.random() <= probaennemy) { // 20% de probabilité
+                                const enemy = createEnemy(j * TILE_SIZE + TILE_SIZE / 2, -i * TILE_SIZE - TILE_SIZE / 2);
+                                enemies.push(enemy);
+
+                            }
+                            break;
+                        case '4': // Croisement de routes
+                            const roadTextureCrossfaur = new THREE.TextureLoader().load('4.png');
+                            const roadMaterialCrossfaur = new THREE.MeshBasicMaterial({ map: roadTextureCrossfaur, transparent: true });
+                            const roadGeometryCrossfaur = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
+                            object = new THREE.Mesh(roadGeometryCrossfaur, roadMaterialCrossfaur);
+                            object.rotation.x = -Math.PI / 2; // Aligner le croisement avec le sol
+                            if (Math.random() <= probaennemy) { // 20% de probabilité
+                                const enemy = createEnemy(j * TILE_SIZE + TILE_SIZE / 2, -i * TILE_SIZE - TILE_SIZE / 2);
+                                enemies.push(enemy);
+
+                            }
+                            break;
+                       case '5': // Croisement de routes
+                            const roadTextureCrossfive= new THREE.TextureLoader().load('5.png');
+                            const roadMaterialCrossfive = new THREE.MeshBasicMaterial({ map: roadTextureCrossfive, transparent: true });
+                            const roadGeometryCrossfive = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
+                            object = new THREE.Mesh(roadGeometryCrossfive, roadMaterialCrossfive);
+                            object.rotation.x = -Math.PI / 2; // Aligner le croisement avec le sol
+                            if (Math.random() <= probaennemy) { // 20% de probabilité
+                                const enemy = createEnemy(j * TILE_SIZE + TILE_SIZE / 2, -i * TILE_SIZE - TILE_SIZE / 2);
+                                enemies.push(enemy);
+
+                            }
+                            break;
+
+                        case '6': // Croisement de routes
+                            const roadTextureCrosssix= new THREE.TextureLoader().load('6.png');
+                            const roadMaterialCrosssix = new THREE.MeshBasicMaterial({ map: roadTextureCrosssix, transparent: true });
+                            const roadGeometryCrosssix = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
+                            object = new THREE.Mesh(roadGeometryCrosssix, roadMaterialCrosssix);
+                            object.rotation.x = -Math.PI / 2; // Aligner le croisement avec le sol
+                            if (Math.random() <= probaennemy) { // 20% de probabilité
+                                const enemy = createEnemy(j * TILE_SIZE + TILE_SIZE / 2, -i * TILE_SIZE - TILE_SIZE / 2);
+                                enemies.push(enemy);
+
+                            }
+                            break;
+              case '7': // Croisement de routes
+                            const roadTextureCrossseven= new THREE.TextureLoader().load('7.png');
+                            const roadMaterialCrossseven = new THREE.MeshBasicMaterial({ map: roadTextureCrossseven, transparent: true });
+                            const roadGeometryCrossseven = new THREE.PlaneGeometry(TILE_SIZE, TILE_SIZE, 1, 1);
+                            object = new THREE.Mesh(roadGeometryCrossseven, roadMaterialCrossseven);
                             object.rotation.x = -Math.PI / 2; // Aligner le croisement avec le sol
                             if (Math.random() <= probaennemy) { // 20% de probabilité
                                 const enemy = createEnemy(j * TILE_SIZE + TILE_SIZE / 2, -i * TILE_SIZE - TILE_SIZE / 2);
@@ -189,32 +254,62 @@ function createEnemy(x, z) {
     return enemy;
 }
 
+function startChrono() {
+    startTime = performance.now();
+    chronoRunning = true;
+}
+
+// Fonction pour arrêter le chronomètre
+function stopChrono() {
+    endTime = performance.now();
+    chronoRunning = false;
+    const elapsedTime = (endTime - startTime) / 1000; // Durée en secondes
+}
+
+
+
+
 function detectCollisions() {
     const playerBoundingBox = new THREE.Box3().setFromObject(character);
 
-    for (let i = 0; i < enemies.length; i++) {
-        const enemyBoundingBox = new THREE.Box3().setFromObject(enemies[i]);
-
+    // Vérifier les collisions avec les ennemis
+    for (const enemy of enemies) {
+        const enemyBoundingBox = new THREE.Box3().setFromObject(enemy);
         if (playerBoundingBox.intersectsBox(enemyBoundingBox)) {
-            speed = 0;
-            break;
+            // Réduire la vitesse lorsque le joueur touche un ennemi
+            speed = 0; // Arrêter le joueur
+            // Ici, vous pouvez ajouter d'autres logiques comme la perte de vie
+            return; // Sortir de la fonction dès qu'une collision avec un ennemi est détectée
         }
     }
 
-    // Vérifier les collisions avec les bâtiments
-    scene.children.forEach((object) => {
-        if (object.userData.type === 'building') {
-            const buildingBoundingBox = new THREE.Box3().setFromObject(object);
+    // Vérifier les collisions avec les objets de la scène
+    for (const object of scene.children) {
+        const objectBoundingBox = new THREE.Box3().setFromObject(object);
 
-            if (playerBoundingBox.intersectsBox(buildingBoundingBox)) {
-                // Gérer la collision avec le bâtiment ici
-                speed = -0;
-                breack();
-                // Autres actions à effectuer en cas de collision avec un bâtiment
+        if (playerBoundingBox.intersectsBox(objectBoundingBox)) {
+            if (object.userData.type === 'building') {
+                speed = 0;
+                // Le joueur a touché un bâtiment de type 'building'
+                
+            } else if (object.userData.type === 'END') {
+                // Le joueur a touché l'objet de fin
+                stopChrono()
+                 // Arrêter le chronomètre
+                // Autres actions à effectuer lorsque le joueur atteint l'objet de fin
+                return; // Sortir de la fonction dès qu'une collision avec l'objet de fin est détectée
             }
         }
-    });
+    }
 }
+
+
+
+
+
+
+
+
 
 
 function updateEnemies() {
@@ -241,6 +336,40 @@ function updateEnemies() {
 }
 
 
+// Fonction pour sauvegarder le meilleur temps dans le localStorage
+function saveBestTime(timeInSeconds) {
+    const bestTime = localStorage.getItem('bestTime');
+
+    if (!bestTime || timeInSeconds < bestTime) {
+        localStorage.setItem('bestTime', timeInSeconds);
+    }
+}
+
+// Fonction pour récupérer et afficher le meilleur temps à l'écran
+function displayBestTime() {
+    const bestTime = localStorage.getItem('bestTime');
+
+    if (bestTime) {
+        const bestTimeDisplay = document.getElementById('bestTimeDisplay');
+        bestTimeDisplay.textContent = `Meilleur temps : ${bestTime.toFixed(1)} secondes`;
+    }
+}
+
+// Fonction pour afficher l'écran figé et le meilleur temps à la fin du jeu
+function showEndScreen(timeInSeconds) {
+    const overlay = document.getElementById('overlay');
+    const bestTimeDisplay = document.getElementById('bestTimeDisplay');
+
+    overlay.style.display = 'block';
+    saveBestTime(timeInSeconds); // Enregistrer le meilleur temps
+    displayBestTime(); // Afficher le meilleur temps
+}
+
+// Fonction pour redémarrer le jeu lors du clic sur le bouton "Recommencer"
+function restartGame() {
+    location.reload(); // Recharger la page pour recommencer le jeu
+}
+
 
 function animate() {
     requestAnimationFrame(animate);
@@ -252,6 +381,19 @@ function animate() {
     updateEnemies(); // Mettre à jour le mouvement des ennemis vers le joueur
 
     const rotationSpeed = 0.05;
+
+// Fonction pour mettre à jour et afficher le chronomètre à l'écran
+function updateChronoDisplay(timeInSeconds) {
+    const chronoDisplay = document.getElementById('chronoDisplay');
+    chronoDisplay.textContent = `Temps écoulé : ${timeInSeconds.toFixed(1)} secondes`;
+}
+
+// Dans la fonction animate(), mettez à jour l'affichage du chronomètre si le chrono est en cours
+if (chronoRunning) {
+    const currentTime = performance.now();
+    const elapsedTimeSeconds = (currentTime - startTime) / 1000;
+    updateChronoDisplay(elapsedTimeSeconds);
+}
 
     // Gestion du mouvement en fonction des touches enfoncées
     const moveDirection = new THREE.Vector3(0, 0, -1); // Direction de base (avancer)
@@ -356,16 +498,15 @@ function handleKeyUp(event) {
             break;
     }
 }
-
 function accelerate() {
-
     if (speed < maxSpeed) {
-        // Si la vitesse est très proche de zéro, la mettre à une valeur minimale pour éviter la multiplication par zéro
         if (Math.abs(speed) < 0.01) {
-            speed = 0.1; // Définir une vitesse minimale si elle est presque nulle
+            speed = 0.1;
+            if (chronoRunning === false) {
+                startChrono(); // Démarrer le chronomètre lorsque le mouvement commence
+            }
         }
-        speed *= accelerationFactor; // Accélération exponentielle
-        // Limiter la vitesse maximale
+        speed *= accelerationFactor;
         if (speed > maxSpeed) {
             speed = maxSpeed;
         }
